c65022295c390786c0e26d64bc84a6dcdf731cef
Omar Daqah <omardakka42@gmail.com>
2021-05-05 10:34:59 +0300

    adding the python folder

---
+++ _python/python_fundamentals/hello_world.py
@@ -0,0 +1,20 @@
+#1
+print("Hello World")
+#2
+name = "Noelle"
+print("Hello", name)
+print("Hello" + name)
+#3
+number = 42
+print("Hello" , number)
+print("Hello " + str(number))
+#4
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1,fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
+
+print(fave_food1.upper())
+print(fave_food2.lower())
+print(name.split())
+print(fave_food2.endswith("a"))
