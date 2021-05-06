1975ff49b3d68164150dc0a55da5f28a78ccc71d
Omar Daqah <omardakka42@gmail.com>
2021-05-06 14:13:40 +0300

    BankAccount assignment

--- _python/OOP/chaining_methods.py
+++ _python/OOP/chaining_methods.py
@@ -21,6 +21,9 @@ class User:
         other_user.account_balance += amount
 
 
+
+
+
 omar = User("Omar", "Omardakka42@gmail.com")
 haitham = User("Haitham", "Haitham@gmail.com")
 ahmad = User("Ahmad", "Ahmad@gmail.com")
