1c6d6be42da8aa4e3bb996afbb2b3910841a69d4
Omar Daqah <omardakka42@gmail.com>
2021-05-06 12:14:28 +0300

    added chaining method

--- _python/OOP/chaining_methods.py
+++ _python/OOP/chaining_methods.py
@@ -24,24 +24,11 @@ class User:
 omar = User("Omar", "Omardakka42@gmail.com")
 haitham = User("Haitham", "Haitham@gmail.com")
 ahmad = User("Ahmad", "Ahmad@gmail.com")
-
-omar.make_deposit(150)
-omar.make_deposit(50)
-omar.make_deposit(300)
-omar.make_withdrawal(200)
-omar.display_user_balance()
-
-haitham.make_deposit(500)
-haitham.make_deposit(1000)
-haitham.make_withdrawal(300)
-haitham.make_withdrawal(200)
-haitham.display_user_balance()
-
-ahmad.make_deposit(700)
-ahmad.make_withdrawal(500)
-ahmad.make_withdrawal(300)
-ahmad.make_withdrawal(200)
-ahmad.display_user_balance()
-
+omar.make_deposit(150).make_deposit(50).make_deposit(
+    300).make_withdrawal(200).display_user_balance()
+haitham.make_deposit(500).make_deposit(1000).make_withdrawal(
+    300).make_withdrawal(200).display_user_balance()
+ahmad.make_deposit(700).make_withdrawal(500).make_withdrawal(
+    300).make_withdrawal(200).display_user_balance()
 omar.transfer_money(ahmad, 300)
 ahmad.display_user_balance()
