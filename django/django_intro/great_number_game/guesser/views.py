a507fa33f3ed9a4d078624346f76a1a215e60aa4
Omar Daqah <omardakka42@gmail.com>
2021-05-19 23:10:46 +0300

    great number game assignment

---
+++ django/django_intro/great_number_game/guesser/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,HttpResponse
+import random
+
+
+def main(request):
+    del request.session["attempt"]
+    if "number" not in request.session:
+        request.session['number'] = 0
+    if "attempt" not in request.session:
+        request.session['attempt'] = 0
+    request.session['number'] = random.randint(1,100)
+    return render(request,"result.html")
+
+def show(request):
+    guessing = request.POST["guess"]
+    request.session["guessingAgain"] = guessing
+    if request.session['number'] > int(request.session["guessingAgain"]):
+        request.session["answer"] = "Too Low"
+    if request.session['number'] < int(request.session["guessingAgain"]):
+        request.session["answer"] = "Too High"
+    if request.session['number'] == int(request.session["guessingAgain"]):
+        request.session["answer"] = "Correct"
+    request.session["attempt"] += 1
+    context = {
+        "number" : request.session["number"],
+        "firstGuess" : request.session["answer"],
+        "attempts" : request.session["attempt"]
+    }
+    
+    return render(request,"result.html", context)
