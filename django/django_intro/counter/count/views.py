2d136262ac948728094c5b8de15c9e7e39d6ddca
Omar Daqah <omardakka42@gmail.com>
2021-05-19 19:08:11 +0300

    update

--- django/django_intro/counter/count/views.py
+++ django/django_intro/counter/count/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def main(request):
     if "counter" not in request.session:
-        request.session["counter"] =0
+        request.session["counter"] = 0
+    if "views" not in request.session:
+        request.session["views"] = 0
     request.session["counter"] += 1 
+    request.session["views"] += 1 
     context = {
-        "temp" : str(request.session["counter"])
+        "counter" : str(request.session["counter"]),
+        "views" : str(request.session["views"])  
     }
     return render(request, "index.html", context)
 
@@ -20,6 +24,5 @@ def destroy(request):
 
 def add(request):
     number_from_form = request.POST["number"]
-    print(number_from_form)
     request.session["counter"] += (int(number_from_form) - 1)
     return redirect('/')
